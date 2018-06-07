5faac3fda8d836ce742feefc5866a4d3086ab044
xuesemofa <xuesemofa@126.com>
2018-06-07 23:18:10 +0800

    添加商品管理

--- src/main/java/org/fx/spgl/controller/SpglController.java
+++ src/main/java/org/fx/spgl/controller/SpglController.java
@@ -2,17 +2,28 @@ package org.fx.spgl.controller;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.layout.AnchorPane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.util.Callback;
 import org.fx.feign.SpglInterface;
 import org.fx.spgl.model.SpglModel;
 import org.fx.urils.*;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
 import java.util.logging.Logger;
 
-public class SpglController {
+public class SpglController implements Initializable {
 
     private static Logger logger = Logger.getLogger(SpglController.class.toString());
     private SpglInterface spglInterface = FeignUtil.feign()
@@ -21,9 +32,93 @@ public class SpglController {
     private AlertUtil alert = new AlertUtil();
 
     @FXML
-    private AnchorPane spzl_index;
+    private TableView spgl_table;
+    @FXML
+    private TableColumn xh;
+    @FXML
+    private TableColumn cname;
+    @FXML
+    private TableColumn jg;
+    @FXML
+    private TableColumn dw;
+    @FXML
+    private TableColumn ge;
+    @FXML
+    private TableColumn pp;
+    @FXML
+    private TableColumn sl;
+    @FXML
+    private TableColumn lm;
+    @FXML
+    private TableColumn sxj_string;
+    @FXML
+    private TableColumn cz;
 
-    public ResponseResult<List<SpglModel>> index(int page) {
+    public void getData(int page) {
+        ObservableList<SpglModel> list = FXCollections.observableArrayList();
+//        映射
+        xh.setCellFactory((col) -> {
+            TableCell<SpglModel, String> cell = new TableCell<SpglModel, String>() {
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    this.setText(null);
+                    this.setGraphic(null);
+                    if (!empty) {
+                        int rowIndex = this.getIndex() + 1;
+                        this.setText(String.valueOf(rowIndex));
+                    }
+                }
+            };
+            return cell;
+        });
+        cname.setCellValueFactory(new PropertyValueFactory("cname"));
+        jg.setCellValueFactory(new PropertyValueFactory("jg"));
+        dw.setCellValueFactory(new PropertyValueFactory("dw"));
+        ge.setCellValueFactory(new PropertyValueFactory("ge"));
+        pp.setCellValueFactory(new PropertyValueFactory("pp"));
+        sl.setCellValueFactory(new PropertyValueFactory("sl"));
+        lm.setCellValueFactory(new PropertyValueFactory("lm"));
+        sxj_string.setCellValueFactory(new PropertyValueFactory("sxj_string"));
+        cz.setCellFactory((col) -> {
+            TableCell<SpglModel, String> cell = new TableCell<SpglModel, String>() {
+                @Override
+                public void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    this.setText(null);
+                    this.setGraphic(null);
+                    if (!empty) {
+                        Button button = new Button("删除");
+                        button.setOnAction(o -> {
+                            AlertUtil alertUtil = new AlertUtil();
+                            boolean b = alertUtil.f_alert_confirmDialog("警告", "是否确定删除?");
+//                            if (b) {
+//                                int i = new SpglController().del(o, this.getTableView().getItems().get(this.getIndex()));
+//                                if (i > 0)
+//                                    alertUtil.f_alert_informationDialog("通知", "成功");
+//                                else
+//                                    alertUtil.f_alert_informationDialog("警告", "失败");
+//                                new SpglController().spgl(pane, 0);
+//                            }
+                        });
+                        button.getStyleClass().add("button");
+                        button.getStyleClass().add("button2");
+                        button.getStyleClass().add("button3");
+                        Button button2 = new Button("修改");
+                        button2.setOnAction(o -> {
+//                            new SpglController().update(o, pane_lout, this.getTableView().getItems().get(this.getIndex()));
+                        });
+                        button2.getStyleClass().add("button");
+                        button2.getStyleClass().add("button2");
+                        HBox hBox = new HBox();
+                        hBox.getChildren().addAll(button, button2);
+                        this.setGraphic(hBox);
+                    }
+                }
+            };
+            return cell;
+        });
+
         ResponseResult<String> result = spglInterface.page(page, 15, StaticToken.getToken());
         if (result.isSuccess()) {
             String json = result.getData().substring(0, result.getData().lastIndexOf("]") + 1);
@@ -32,16 +127,16 @@ public class SpglController {
                 });
                 String s = result.getData().substring(result.getData().lastIndexOf("]") + 1, result.getData().length());
                 StaticToken.setToken(s);
-                return new ResponseResult<>(true, "成功", beanList);
+                list.addAll(beanList);
+                spgl_table.setItems(list);
             } catch (IOException e) {
                 e.printStackTrace();
-                return new ResponseResult<>(false, "数据转换错误", null);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return new ResponseResult<>(false, "获取数据失败", null);
             }
-        } else {
-            return new ResponseResult<>(false, result.getMessage(), null);
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        getData(0);
     }
 }
--- src/main/java/org/fx/spgl/view/SpglView.java
+++ src/main/java/org/fx/spgl/view/SpglView.java
@@ -1,20 +1,11 @@
 package org.fx.spgl.view;
 
 import com.sun.javafx.robot.impl.FXRobotHelper;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import org.fx.spgl.controller.SpglController;
-import org.fx.spgl.model.SpglModel;
-import org.fx.urils.ResponseResult;
-
-import java.util.List;
 
 /**
  * @author ld
@@ -30,24 +21,6 @@ public class SpglView {
         AnchorPane node = (AnchorPane) root2.lookup("#spzl_index");
         node.setPrefWidth(lookup.getWidth());
         node.setPrefHeight(lookup.getHeight());
-        TableView tableView = (TableView) root2.lookup("#spgl_table");
-
-        tableView.getVisibleLeafColumn(1).setCellValueFactory(new PropertyValueFactory<SpglModel,String>("cname"));
-//        ((TableColumn) root2.lookup("#spgl_table_cname")).setCellValueFactory(new PropertyValueFactory<>("cname"));
-//            ((TableColumn) columns.get(1)).setCellValueFactory(new PropertyValueFactory<>("cname"));
-//            ((TableColumn) columns.get(2)).setCellValueFactory(new PropertyValueFactory<>("jg"));
-//            ((TableColumn) columns.get(3)).setCellValueFactory(new PropertyValueFactory<>("dw"));
-//            ((TableColumn) columns.get(4)).setCellValueFactory(new PropertyValueFactory<>("gg"));
-//            ((TableColumn) columns.get(5)).setCellValueFactory(new PropertyValueFactory<>("pp"));
-//            ((TableColumn) columns.get(6)).setCellValueFactory(new PropertyValueFactory<>("sl"));
-//            ((TableColumn) columns.get(7)).setCellValueFactory(new PropertyValueFactory<>("lm"));
-//            ((TableColumn) columns.get(8)).setCellValueFactory(new PropertyValueFactory<>("sxj_string"));
-
-            ObservableList<Object> arrayList = FXCollections.observableArrayList();
-            ResponseResult<List<SpglModel>> result = new SpglController().index(0);
-            if (result.isSuccess())
-                arrayList.add(result.getData());
-            tableView.setItems(arrayList);
         lookup.getChildren().clear();
         lookup.getChildren().addAll(root2);
     }
